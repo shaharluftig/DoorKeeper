f4fcd285c21a7138fa2f304a70bcb505c0ee73e3
Luftig <shaharluftig@gmail.com>
2022-01-01 19:06:15 +0200

    Added async compute, added unknown person handling

--- Config/__init__.py
+++ Config/__init__.py
@@ -1,9 +1,36 @@
+import os
+
 from DataProviders.FSProvider import FSProvider
 
 # Env Setup
-TELEGRAM_BOT_TOKEN = ""
-TELEGRAM_BOT_CHAT_ID = ""
+ENV = "dev"
 
-IP_CAMERA_URL = ""
-INFER_PROVIDERS = True
-PROVIDERS = [FSProvider("./Images")]
+
+class DefaultConfig:
+    telegram_bot_token = ""
+    telegram_bot_chat_id = ""
+    mongo_collection = "facedb"
+
+    infer_providers = True
+    providers = [FSProvider("./Images")]
+
+
+class ProdConfig(DefaultConfig):
+    ip_camera_url = ""
+    mongo_username = os.environ.get('MONGODB_USERNAME')
+    mongo_password = os.environ.get('MONGODB_PASSWORD')
+    mongo_host = os.environ.get('MONGODB_HOSTNAME')
+    mongo_port = os.environ.get('MONGODB_PORT')
+    mongo_db = os.environ.get('MONGODB_DATABASE')
+
+
+class DevConfig(DefaultConfig):
+    ip_camera_url = 0
+    mongo_username = "admin"
+    mongo_password = "admin"
+    mongo_host = "localhost"
+    mongo_port = 27017
+    mongo_db = "facedb"
+
+
+config = ProdConfig if ENV == "prod" else DevConfig
--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import face_utils
-from Config import TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_CHAT_ID, IP_CAMERA_URL, INFER_PROVIDERS, PROVIDERS
-from Config.telegram_bot_config import SLEEP_INTERVAL
+import asyncio
+
+from Config import config
 from Connectors.FacesDB import FacesDB
 from DoorKeeper import DoorKeeper
 from OutputStreams.TelegramBot import TelegramBot
+from face_utils import infer_providers
 
 
-def infer_providers(providers):
-    user_faces = []
-    for provider in providers:
-        user_faces += provider.get_faces_data()
-    return user_faces
+async def main():
+    db = FacesDB(host=config.mongo_host, port=config.mongo_port, username=config.mongo_username,
+                 password=config.mongo_password, db=config.mongo_db, collection=config.mongo_collection)
+    if config.infer_providers:
+        infer_providers(config.providers, db)
+
+    faces_data = db.collect_all_data()
+    telegram_bot = TelegramBot(config.telegram_bot_token, config.telegram_bot_chat_id)
+    await DoorKeeper(faces_data, ip_cam_url=0, output_stream=telegram_bot).start_recognizing()
 
 
 if __name__ == '__main__':
-    db = FacesDB(*face_utils.get_mongo_connection_param())
-    if INFER_PROVIDERS:
-        data = infer_providers(PROVIDERS)
-        db.add_complex_object(data, many=True)
-
-    faces_data = db.collect_all_data()
-    output_stream = TelegramBot(TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_CHAT_ID, SLEEP_INTERVAL)
-    DoorKeeper(IP_CAMERA_URL, output_stream).start_recognizing(faces_data)
+    asyncio.run(main())
